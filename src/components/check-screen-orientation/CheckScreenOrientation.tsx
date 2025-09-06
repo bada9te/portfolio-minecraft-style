bc2814240b73cefaf1e6c6ce2737c4d5238e8198
Bohdan Teliepov <97344806+bada9te@users.noreply.github.com>
2025-09-06 18:46:21 +0300

    ui updates

--- src/components/check-screen-orientation/CheckScreenOrientation.tsx
+++ src/components/check-screen-orientation/CheckScreenOrientation.tsx
@@ -9,12 +9,12 @@ export default function CheckScreenOrientation() {
         function checkOrientation() {
             if (window.matchMedia("(orientation: portrait)").matches) {
                 // Portrait: show message, hide content
-                message.classList.add("opacity-100");
-                message.classList.remove("opacity-0");
+                message?.classList.add("flex");
+                message?.classList.remove("hidden");
             } else {
                 // Landscape: hide message, show content
-                message.classList.add("opacity-0");
-                message.classList.remove("opacity-100");
+                message?.classList.add("hidden");
+                message?.classList.remove("flex");
             }
         }
 
@@ -33,8 +33,8 @@ export default function CheckScreenOrientation() {
     }, []);
 
     return (
-        <span className="portrait-message absolute inset-0 bg-black opacity-0 font-minecraft top-0 left-0 w-full h-full flex justify-center items-center text-white text-center z-50">
-      Please rotate your device to landscape mode...
-    </span>
+        <span className="portrait-message absolute inset-0 bg-black font-minecraft top-0 left-0 w-full h-full justify-center items-center text-white text-center z-50">
+          Please rotate your device to landscape mode...
+        </span>
     );
 }
