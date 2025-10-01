033aa02cc4f7e0ce213a25887108c5665a2b7139
Bohdan Teliepov <97344806+bada9te@users.noreply.github.com>
2025-10-01 12:58:03 -0600

    manifest.ts

---
+++ src/app/manifest.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'bada9te.dev',
+        short_name: 'b9t portfolio',
+        description: 'Minecraft-styled bada9te.dev portfolio website',
+        start_url: '/',
+        display: 'fullscreen',
+    }
+}
